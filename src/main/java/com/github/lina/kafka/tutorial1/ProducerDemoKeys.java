94f6f840ba476837019a98d669655b2f3c49fd98
Anil Kulkarni <anil77k@gmail.com>
2019-09-10 08:40:06 -0700

    Initial commit with basic producer code.

---
+++ src/main/java/com/github/lina/kafka/tutorial1/ProducerDemoKeys.java
@@ -0,0 +1,52 @@
+package com.github.lina.kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class ProducerDemoKeys {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        final Logger logger = LoggerFactory.getLogger(ProducerDemoKeys.class);
+        String bootstrapServer = "127.0.0.1:9092";
+
+        //Create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //Create Producer
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<String, String>(properties);
+        for(int i =1; i<=10 ; i++) {
+            String topic = "first_topic";
+            String value = "ProducerDemoWithCallback_" + Integer.toString(i);
+            String key = "id_" + Integer.toString(i);
+            ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>(topic,key, value);
+            logger.info("Key: " + key);
+            //Send Data
+            kafkaProducer.send(producerRecord, new Callback() {
+                public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                    //executes everytime a record is sent successfully or an exception is thrown
+                    if (e == null) {
+                        //record successfulyl sent
+                        logger.info("Received new metadata: \n" +
+                                "Topic: " + recordMetadata.topic() + "\n" +
+                                "Partition: " + recordMetadata.partition() + "\n" +
+                                "Offset: " + recordMetadata.offset() + "\n" +
+                                "Timestamp: " + recordMetadata.timestamp());
+                    } else {
+                        logger.error("Error while producing", e);
+                    }
+                }
+            }).get(); //To block .send() and make it synchronous - dont do in production
+        }
+        //flush data
+        kafkaProducer.flush();
+        //flush and close producer
+        kafkaProducer.close();
+    }
+}
---
+++ src/main/java/com/github/lina/kafka/tutorial1/ProducerDemoWithCallback.java
@@ -0,0 +1,45 @@
+package com.github.lina.kafka.tutorial1;
+
+import org.apache.kafka.clients.producer.*;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class ProducerDemoWithCallback {
+    public static void main(String[] args) {
+        final Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
+        String bootstrapServer = "127.0.0.1:9092";
+
+        //Create Producer Properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServer);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        //Create Producer
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<String, String>(properties);
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<String, String>("first_topic","ProducerDemoWithCallback");
+        //Send Data
+        kafkaProducer.send(producerRecord, new Callback() {
+            public void onCompletion(RecordMetadata recordMetadata, Exception e) {
+                //executes everytime a record is sent successfully or an exception is thrown
+                if(e == null){
+                    //record successfulyl sent
+                    logger.info("Received new metadata: \n" +
+                            "Topic: " + recordMetadata.topic() + "\n" +
+                            "Partition: " + recordMetadata.partition() + "\n" +
+                            "Offset: " + recordMetadata.offset() + "\n" +
+                            "Timestamp: " + recordMetadata.timestamp());
+                } else {
+                    logger.error("Error while producing", e);
+                }
+            }
+        });
+        //flush data
+        kafkaProducer.flush();
+        //flush and close producer
+        kafkaProducer.close();
+    }
+}
