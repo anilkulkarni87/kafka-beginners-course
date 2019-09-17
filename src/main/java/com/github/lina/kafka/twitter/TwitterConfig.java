0a40accd6f8d577f83bb3bb3cbd2eab34ddf1b58
Anil Kulkarni <anil77k@gmail.com>
2019-09-17 01:01:24 -0700

    Adding basic Twitter Client and Producer code.

---
+++ src/main/java/com/github/lina/kafka/twitter/TwitterConfig.java
@@ -0,0 +1,9 @@
+package com.github.lina.kafka.twitter;
+
+public final class TwitterConfig {
+    public static final String CONSUMER_KEY = "7ZZe6j6mmsgMyTzH6Lq9pC1ZF";
+    public static final String CONSUMER_SECRET = "6Efvf1NgRAAtcz0fFMA6o5MzgeMPIt4sX5j7fuOm1sByYAUTge";
+    public static final String TOKEN = "15154170-JtM1PEbanQYpW4EYCfFnQHK57Au5gbVjNHQ64QADV";
+    public static final String TOKEN_SECRET = "1OYeURq9hns8jN3kkoEZfvhPWzoePnyCjQQ3JfvN2USLD";
+    public static final String HASHTAG ="#Nordstrom";
+}
